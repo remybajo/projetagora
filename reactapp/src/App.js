7612b7f2f0f849ad0687896fe27cf7822fe91627
Sir Alex <amadoudiaw@hotmail.com>
2021-10-27 13:47:54 +0200

    front3

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,43 +1,38 @@
-import React, { useState } from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import { Provider} from 'react-redux';
-import { connect} from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import token from './reducers/token'
-import Accueil from './Accueil';
-import Inscription from './inscription';
-import Profilcomp from './profilcomp';
-import Thematique from './Thematique';
-import nouvelArticle from './nouvelArticle';
-import Test from './Test';
-import Publication from './publication';
+import { Provider } from "react-redux";
+import { connect } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import token from "./reducers/token";
+import Accueil from "./Accueil";
+import Inscription from "./inscription";
+import Profilcomp from "./profilcomp";
+import Thematique from "./Thematique";
+import nouvelArticle from "./nouvelArticle";
+import Test from "./Test";
+import Publication from "./publication";
+import CompleterProfil from "./completerProfil";
 //import Cookies from 'js-cookie';
 
-
-
-const store = createStore(combineReducers({token}))
+const store = createStore(combineReducers({ token }));
 
 function App(props) {
- 
   return (
-
-    <Provider store={store} >
+    <Provider store={store}>
       <Router>
         <Switch>
           <Route component={Accueil} path="/" exact />
           <Route component={Test} path="/test" exact />
           <Route component={Inscription} path="/inscription" exact />
           <Route component={Profilcomp} path="/profilcomp" exact />
-          <Route component={Publication} path="/publication" exact/>
-          <Route component={Thematique} path="/Thematique" exact/>
-          <Route component={nouvelArticle} path="/nouvelArticle" exact/>
+          <Route component={Publication} path="/publication" exact />
+          <Route component={Thematique} path="/Thematique" exact />
+          <Route component={nouvelArticle} path="/nouvelArticle" exact />
+          <Route component={CompleterProfil} path="/completerProfil" exact />
         </Switch>
       </Router>
-      
-   
-
     </Provider>
   );
 }
--- reactapp/src/Thematique.js
+++ reactapp/src/Thematique.js
@@ -1,14 +1,7 @@
 import React from "react";
-import {
-  Col,
-  Layout,
-  Card,
-  Row,
-} from "antd";
+import { Col, Layout, Card, Row } from "antd";
 import "antd/dist/antd.css";
-import {
-  PlusOutlined,
-} from "@ant-design/icons";
+import { PlusOutlined, PlusCircleFilled } from "@ant-design/icons";
 const { Header, Content, Footer, Sider } = Layout;
 
 const { Meta } = Card;
@@ -18,70 +11,125 @@ const { Meta } = Card;
 
 function Thematique(props) {
   return (
-      <>
-    <Row justify="center" >
-        <Col span="4">
-        </Col>
+    <>
+      <Row justify="center">
+        <Col span="4"></Col>
         <Col span="16">
-            <h1  style={{ backgroundColor: "#214C74", color: "white", width: "100%", textAlign: "center", height: 50, justifyContent: "center", marginTop: 50}}>
-                Thématiques
-            </h1>
+          <h1
+            style={{
+              backgroundColor: "#214C74",
+              color: "white",
+              width: "100%",
+              textAlign: "center",
+              height: 50,
+              justifyContent: "center",
+              marginTop: 50,
+            }}
+          >
+            Thématiques
+          </h1>
         </Col>
-        <Col span="4">
-        </Col>
-        </Row>
-        <Row justify="start" display="flex">
+        <Col span="4"></Col>
+      </Row>
+      <Row justify="start" display="flex">
         <Card
-                  
-                  style={{ 
-                  width: "100%", 
-                  margin:'15px', 
-                  display:'flex',
-                  flexDirection: 'row',
-                  justifyContent:'space-between'}}
-                  cover={
-                  <img alt="example" src="https://mcdn.wallpapersafari.com/medium/76/5/HJEso9.jpg" style={{width: 200}} />}
+          style={{
+            width: "100%",
+            margin: "15px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+          cover={
+            <img
+              alt="example"
+              src="https://mcdn.wallpapersafari.com/medium/76/5/HJEso9.jpg"
+              style={{ width: 200 }}
+            />
+          }
         >
-        <div style={{justifyContent: "center", width: "75em", textAlign: "center"}}>
+          <div
+            style={{
+              justifyContent: "center",
+              width: "75em",
+              textAlign: "center",
+            }}
+          >
             <h3>titre</h3>
             <h3>description</h3>
-        </div>
+          </div>
         </Card>
         <Card
-                  
-                  style={{ 
-                  width: "100%", 
-                  margin:'15px', 
-                  display:'flex',
-                  flexDirection: 'row',
-                  justifyContent:'space-between'}}
-                  cover={
-                  <img alt="example" src="https://mcdn.wallpapersafari.com/medium/76/5/HJEso9.jpg" style={{width: 200}} />}
+          style={{
+            width: "100%",
+            margin: "15px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+          cover={
+            <img
+              alt="example"
+              src="https://mcdn.wallpapersafari.com/medium/76/5/HJEso9.jpg"
+              style={{ width: 200 }}
+            />
+          }
         >
-        <div style={{justifyContent: "center", width: "75em", textAlign: "center"}}>
-            <h3>titre</h3>
-            <h3>description</h3>
-        </div>
+          <div
+            style={{
+              justifyContent: "center",
+              width: "75em",
+              textAlign: "center",
+              display: "flex",
+            }}
+          >
+            <Col span="22">
+              <h3>titre</h3>
+              <h3>description</h3>
+            </Col>
+            <Col span="2">
+              <PlusCircleFilled
+                style={{ fontSize: "30px", color: "#214C74" }}
+              />
+            </Col>
+          </div>
         </Card>
         <Card
-                  
-                  style={{ 
-                  width: "100%", 
-                  margin:'15px', 
-                  display:'flex',
-                  flexDirection: 'row',
-                  justifyContent:'space-between'}}
-                  cover={
-                  <img alt="example" src="https://mcdn.wallpapersafari.com/medium/76/5/HJEso9.jpg" style={{width: 200}} />}
+          style={{
+            width: "100%",
+            margin: "15px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+          cover={
+            <img
+              alt="example"
+              src="https://mcdn.wallpapersafari.com/medium/76/5/HJEso9.jpg"
+              style={{ width: 200 }}
+            />
+          }
         >
-        <Row style={{justifyContent: "center", width: "75em", textAlign: "center", display: "flex"}} >
-          <Col span="22"><h3>titre</h3></Col>
-          <Col span="2"><PlusOutlined/></Col>
-          
-        </Row>
+          <div
+            style={{
+              justifyContent: "center",
+              width: "75em",
+              textAlign: "center",
+              display: "flex",
+            }}
+          >
+            <Col span="22">
+              <h3>titre</h3>
+              <h3>description</h3>
+            </Col>
+            <Col span="2">
+              <PlusCircleFilled
+                style={{ fontSize: "30px", color: "#214C74" }}
+              />
+            </Col>
+          </div>
         </Card>
-
-    </Row>
+      </Row>
     </>
   );
 }
