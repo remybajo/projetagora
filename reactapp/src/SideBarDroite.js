47a56c6d4e8498ba4f50a314da1828143c5ae644
Sir Alex <amadoudiaw@hotmail.com>
2021-11-02 09:05:02 +0100

    home5

--- reactapp/src/SideBarDroite.js
+++ reactapp/src/SideBarDroite.js
@@ -1,8 +1,7 @@
-
 import { Link, Redirect } from "react-router-dom";
 import React, { useState, useEffect, useRef } from "react";
 import Inscription from "./inscription";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 import {
   Button,
   Layout,
@@ -14,7 +13,8 @@ import {
   Row,
   Col,
   Tabs,
-  Modal
+  Modal,
+  Affix,
 } from "antd";
 import "antd/dist/antd.css";
 import {
@@ -40,36 +40,30 @@ const { SubMenu } = Menu;
 const { Meta } = Card;
 const { TabPane } = Tabs;
 
-
 function SideBarDroite(props) {
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isConnect, setIsConnect] = useState(false)
-  const [isConnectProfil, setIsConnectProfil] = useState(false)
-  const [theme, setTheme] = useState("")
+  const [isConnect, setIsConnect] = useState(false);
+  const [isConnectProfil, setIsConnectProfil] = useState(false);
+  const [theme, setTheme] = useState("");
+  const [top, setTop] = useState(10);
 
   //Bade de donnée Data
-const themeData = [
-  
-  "Emploi",
-  "Education",
-  "Politique",
-  "Evenement",
-  "Environnement",
-  "Sport",
-  "Tourisme",
-  "Tu as remarqué ?"
-
-]
-
-
+  const themeData = [
+    "Emploi",
+    "Education",
+    "Politique",
+    "Evenement",
+    "Environnement",
+    "Sport",
+    "Tourisme",
+    "Tu as remarqué ?",
+  ];
 
   var findClickTheme = (theme) => {
-    setTheme(theme)
-
-    
-  }
-  if (theme){
-   return <Redirect to={`/pageTheme/${theme}`} />
+    setTheme(theme);
+  };
+  if (theme) {
+    return <Redirect to={`/pageTheme/${theme}`} />;
   }
 
   var showModal = () => {
@@ -85,76 +79,83 @@ const themeData = [
   };
 
   var handleClick = async () => {
-    if (props.token == null){
-    showModal()  
- } else{
-   setIsConnect(true)
-  }}
+    if (props.token == null) {
+      showModal();
+    } else {
+      setIsConnect(true);
+    }
+  };
 
-  if (isConnect){
+  if (isConnect) {
     return <Redirect to="/pageprofil" />;
   }
 
   var handleClickPubli = (e) => {
-    if (props.token == null){
-      showModal()  
-   } else{
-     setIsConnectProfil(true)
-    }}
-  
-    if (isConnectProfil){
-      return <Redirect to="/nouvelPublication" />;
+    if (props.token == null) {
+      showModal();
+    } else {
+      setIsConnectProfil(true);
     }
-  
+  };
 
-  
+  if (isConnectProfil) {
+    return <Redirect to="/nouvelPublication" />;
+  }
 
+  var publiTheme = themeData.map((theme, i) => {
+    return (
+      <Menu.Item onClick={() => findClickTheme(theme)} key="i">
+        {theme}
+      </Menu.Item>
+    );
+  });
 
-    var publiTheme = themeData.map((theme, i) => {
-      return (
-        <Menu.Item onClick={() => findClickTheme(theme)} key="i">{theme}</Menu.Item>
-      )})
- 
   return (
     <Sider className="site-layout-background">
-      <Modal title="connexion/inscription" style={{ displayflex: 1, width: 150 }}
-
-visible={isModalVisible}
-onOk={handleOk}
-onCancel={handleCancel}>
-<Inscription /> </Modal>
-      {" "}
-      <Menu
-        style={{ width: 200 }}
-        defaultSelectedKeys={["1"]}
-        // defaultOpenKeys={["sub1"]}
+      <Modal
+        title="connexion/inscription"
+        style={{ displayflex: 1, width: 150 }}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
       >
-        <Menu.Item key="1" icon={<MailOutlined />}>
-          <Link to="/">Accueil</Link>
-        </Menu.Item>
-        <SubMenu key="sub1" icon={<AppstoreOutlined />} title="Thématique">
-          {publiTheme}
-        </SubMenu>
-        <Menu.Item  onClick={() => handleClick()} key="2" icon={<CalendarOutlined  />}  >
-        Mon compte
-        </Menu.Item>
+        <Inscription />{" "}
+      </Modal>{" "}
+      <Affix offsetTop={top}>
+        <Menu
+          style={{ width: 200 }}
+          defaultSelectedKeys={["1"]}
+          // defaultOpenKeys={["sub1"]}
+        >
+          <Menu.Item key="1" icon={<MailOutlined />}>
+            <Link to="/">Accueil</Link>
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<AppstoreOutlined />} title="Thématique">
+            {publiTheme}
+          </SubMenu>
+          <Menu.Item
+            onClick={() => handleClick()}
+            key="2"
+            icon={<CalendarOutlined />}
+          >
+            Mon compte
+          </Menu.Item>
 
-        <Menu.Item  onClick={() => handleClickPubli()} key="link" icon={<EditOutlined />}>
-          
-        Nouvelle publication
-          
-        </Menu.Item>
-      </Menu>
+          <Menu.Item
+            onClick={() => handleClickPubli()}
+            key="link"
+            icon={<EditOutlined />}
+          >
+            Nouvelle publication
+          </Menu.Item>
+        </Menu>
+      </Affix>
     </Sider>
   );
 }
 
-function mapStateToProps(state){
-  return {token:state.token}
+function mapStateToProps(state) {
+  return { token: state.token };
 }
 
-export default connect(
-  mapStateToProps,
-  null,
- 
-)(SideBarDroite) ;
+export default connect(mapStateToProps, null)(SideBarDroite);
--- reactapp/src/nouvelPublication.js
+++ reactapp/src/nouvelPublication.js
@@ -1,13 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "./nouvelPublication.css";
-import {Layout, Menu, Button, Image, Empty, Cascader, Input, Space, Row, Col, Form } from "antd";
+import {
+  Layout,
+  Menu,
+  Button,
+  Image,
+  Empty,
+  Cascader,
+  Input,
+  Space,
+  Row,
+  Col,
+  Form,
+} from "antd";
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 // reactstrap pour le moment utilisé pour le modal avec les images en provenance de l'APIK
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle } from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
 
 import EnTete from "./EnTete";
 import SideBarDroite from "./SideBarDroite";
@@ -25,18 +47,17 @@ function NouvelPublication(props) {
   const [theme, setTheme] = useState();
   const [redir, setRedir] = useState(false);
 
-
   // hook d'état pour gestion de l'image
-  const apiKey = "23345038-9d4a0f31be7a8f5a5e2bfc293"
+  const apiKey = "23345038-9d4a0f31be7a8f5a5e2bfc293";
   const [mot_Cle, setMot_Cle] = useState("");
-  const [foundPictures, setFoundPictures] = useState([])
-  const [modal, setModal] = useState(false)
-  const [pictureSelected, setPictureSelected] = useState("")
+  const [foundPictures, setFoundPictures] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [pictureSelected, setPictureSelected] = useState("");
   const [validatePicture, setValidatePicture] = useState(false);
-  const [id, setId] = useState()
+  const [id, setId] = useState();
   var illustration;
-  var border = {border:""};
-  var idP = ''
+  var border = { border: "" };
+  var idP = "";
 
   useEffect(() => {
     var dateKnow = async () => {
@@ -62,49 +83,46 @@ function NouvelPublication(props) {
     console.log("et dans body?", body);
     // if (body.result == true) {
     //   props.addPubliToken(body.publiToken);
-      idP = body.id
-      // return <Redirect to="/publication" />
-      
-      if (body.result == true) {
-        setRedir(true)}
-        setId(idP) 
-        
+    idP = body.id;
+    // return <Redirect to="/publication" />
+
+    if (body.result == true) {
+      setRedir(true);
+    }
+    setId(idP);
+
     //sauvegarde image
     const sendImage = async () => {
-
       var image = new FormData();
-      image.append('image', {
+      image.append("image", {
         uri: pictureSelected,
-        type: 'image/jpeg',
-        name: 'image.jpg',
+        type: "image/jpeg",
+        name: "image.jpg",
       });
-      
+
       let rawResponse = await fetch("/publications/upload", {
-      method: 'post',
-      body: image
-      })
-     }  
-      
+        method: "post",
+        body: image,
+      });
     };
+  };
 
-            if(id){
-          console.log('id', id)
-      return <Redirect to={`/publication/${id}`}/>}
+  if (id) {
+    console.log("id", id);
+    return <Redirect to={`/publication/${id}`} />;
+  }
 
+  /* return <Redirect to={`/publication/${id}`} />} */
 
-/* return <Redirect to={`/publication/${id}`} />} */
-
-
-  // var cherchePubli = async () => {  
+  // var cherchePubli = async () => {
   //   setRedir(true)
   // if (redir == true) {
   //   console.log("ICI APPARAISSENT LES SAINTS PROPS!!!", id);
-    // const actual = await fetch (`/selectedPublication?id=${id}`)
-    // const Ractual = await actual.json()
-    //     setId(idP)
-    //   console.log("idd", idP)
-    // return <Redirect to={`/publication/${id}`} />;
-  
+  // const actual = await fetch (`/selectedPublication?id=${id}`)
+  // const Ractual = await actual.json()
+  //     setId(idP)
+  //   console.log("idd", idP)
+  // return <Redirect to={`/publication/${id}`} />;
 
   const options = [
     {
@@ -156,67 +174,84 @@ function NouvelPublication(props) {
 
   // affichage des images en fonction du mot clé
   var handlePictureRequest = async () => {
-    var keyword = mot_Cle.replace(/\s/g, "|")
+    var keyword = mot_Cle.replace(/\s/g, "|");
     setModal(true);
     console.log("keyword", keyword);
-    var rawResponse = await fetch(`https://pixabay.com/api/?key=${apiKey}&image_type=photo&lang=fr&orientation=horizontal&safesearch=true&q=${keyword}`);
+    var rawResponse = await fetch(
+      `https://pixabay.com/api/?key=${apiKey}&image_type=photo&lang=fr&orientation=horizontal&safesearch=true&q=${keyword}`
+    );
     var response = await rawResponse.json();
-    console.log("log du response : ",response);
+    console.log("log du response : ", response);
     setFoundPictures(response.hits);
-    console.log("log du hits : ",response.hits[0].webformatURL)
-
-  }
+    console.log("log du hits : ", response.hits[0].webformatURL);
+  };
 
   // Validation de l'image sélectionnée
   var handlePictureClick = (img) => {
-    console.log("clicked picture: ", img)
+    console.log("clicked picture: ", img);
     //console.log("access style: ", listPictures[0].props.style)
-    
-    for (var i=0; i<listPictures.length; i++){
-      if(listPictures[i].props.src == img) {
-        setPictureSelected(img)
-        console.log("listpicture src : ",listPictures[i].props.src)
-        border = {border:'1px solid red'};
+
+    for (var i = 0; i < listPictures.length; i++) {
+      if (listPictures[i].props.src == img) {
+        setPictureSelected(img);
+        console.log("listpicture src : ", listPictures[i].props.src);
+        border = { border: "1px solid red" };
         console.log(border);
       }
     }
-    
+  };
+
+  var listPictures = foundPictures.map((picture, i) => {
+    return (
+      <img
+        key={i}
+        src={picture.webformatURL}
+        onClick={() => {
+          handlePictureClick(picture.webformatURL);
+        }}
+        style={{ border, width: "400px" }}
+      />
+    );
+  });
+
+  if (pictureSelected && validatePicture) {
+    var illustration = (
+      <Card style={{ width: "640px", height: "360px" }}>
+        <CardImg
+          width="100%"
+          height="100%"
+          src={pictureSelected}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardTitle tag="h5"></CardTitle>
+          <CardText></CardText>
+        </CardBody>
+      </Card>
+    );
   }
-
-  var listPictures = foundPictures.map((picture,i) => {
-    return (<img key={i} src={picture.webformatURL} onClick={() => {handlePictureClick(picture.webformatURL) }} style={{border, width:"400px"}}/>)
-  })
-
-  if (pictureSelected && validatePicture ){
-    var illustration = 
-        
-    <Card style={{width:"640px", height:"360px"}}>
-      <CardImg width="100%" height="100%" src={pictureSelected} alt="Card image cap" />
-      <CardBody>
-        <CardTitle tag="h5"></CardTitle>
-        <CardText></CardText>
-      </CardBody>
-      </Card>;
-    
-      
-  }
-
 
   return (
     <div style={{ justifyContent: "center" }}>
       <EnTete />
 
       <Row>
-      <Col span={4}>
-      <SideBarDroite />
-      </Col>
+        <Col span={4}>
+          <SideBarDroite />
+        </Col>
 
-      <Col span={16} align="center">
-      <div style={{border: "1px solid black", width: "640px", height:"360px", display:"center"}}>
-        {illustration}
-      </div>
-      </Col>
-
+        <Col span={16} align="center">
+          <div
+            style={{
+              border: "1px solid black",
+              width: "640px",
+              height: "360px",
+              display: "center",
+            }}
+          >
+            {illustration}
+          </div>
+        </Col>
       </Row>
 
       <div className="montimer">
@@ -236,27 +271,46 @@ function NouvelPublication(props) {
             allowClear
             enterButton="Ajouter un mot-clé"
             size="large"
-            
             onSearch={onSearch}
           />
         </Space>
-        <Form >
-        <Form.Item>
-          <Input placeholder="Tapez un mot-clé" onChange={(e) => setMot_Cle(e.target.value)}/>
-          <Button type="primary" onClick={() => handlePictureRequest()}>Valider</Button>
-        </Form.Item>
+        <Form>
+          <Form.Item>
+            <Input
+              placeholder="Tapez un mot-clé"
+              onChange={(e) => setMot_Cle(e.target.value)}
+            />
+            <Button type="primary" onClick={() => handlePictureRequest()}>
+              Valider
+            </Button>
+          </Form.Item>
         </Form>
 
         <Modal isOpen={modal} toggle={modal} className="">
-        <ModalHeader toggle={modal}> Choisissez une image pour le sujet "{mot_Cle}" 
-        <Button color="primary" onClick={() => {setModal(!modal); setValidatePicture(true)}}>Valider la selection</Button>{' '}
-          <Button color="secondary" onClick={() => {setModal(!modal); setValidatePicture(false)}} >Annuler</Button>
-        </ModalHeader>
-        <ModalBody style={{width:"200px"}}>
-        {listPictures}
-        </ModalBody>
-      </Modal>
-
+          <ModalHeader toggle={modal}>
+            {" "}
+            Choisissez une image pour le sujet "{mot_Cle}"
+            <Button
+              color="primary"
+              onClick={() => {
+                setModal(!modal);
+                setValidatePicture(true);
+              }}
+            >
+              Valider la selection
+            </Button>{" "}
+            <Button
+              color="secondary"
+              onClick={() => {
+                setModal(!modal);
+                setValidatePicture(false);
+              }}
+            >
+              Annuler
+            </Button>
+          </ModalHeader>
+          <ModalBody style={{ width: "200px" }}>{listPictures}</ModalBody>
+        </Modal>
       </div>
       <Input
         className="description"
