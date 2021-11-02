5e2ad2f7077ec09adb0c6e77827bfe6431c0e3af
remy <remy@MacBook-Air-de-remy.local>
2021-11-02 15:40:27 +0100

    resolution bug thematique

--- reactapp/src/SideBarDroite.js
+++ reactapp/src/SideBarDroite.js
@@ -1,7 +1,8 @@
+
 import { Link, Redirect } from "react-router-dom";
 import React, { useState, useEffect, useRef } from "react";
 import Inscription from "./inscription";
-import { connect } from "react-redux";
+import { connect } from 'react-redux'
 import {
   Button,
   Layout,
@@ -13,8 +14,7 @@ import {
   Row,
   Col,
   Tabs,
-  Modal,
-  Affix,
+  Modal
 } from "antd";
 import "antd/dist/antd.css";
 import {
@@ -40,32 +40,38 @@ const { SubMenu } = Menu;
 const { Meta } = Card;
 const { TabPane } = Tabs;
 
+
 function SideBarDroite(props) {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isConnect, setIsConnect] = useState(false)
+  const [isConnectProfil, setIsConnectProfil] = useState(false)
+ // const [theme, setTheme] = useState("")
+
+  //Base de donnée Data
+const themeData = [
   
-  const [isConnect, setIsConnect] = useState(false);
-  const [isConnectProfil, setIsConnectProfil] = useState(false);
-  const [theme, setTheme] = useState("");
-  const [top, setTop] = useState(10);
+  "Emploi",
+  "Education",
+  "Politique",
+  "Evenement",
+  "Environnement",
+  "Sport",
+  "Tourisme",
+  "Tu as remarqué ?"
 
-  //Bade de donnée Data
-  const themeData = [
-    "Emploi",
-    "Education",
-    "Politique",
-    "Evenement",
-    "Environnement",
-    "Sport",
-    "Tourisme",
-    "Tu as remarqué ?",
-  ];
+]
 
-  var findClickTheme = (theme) => {
-    setTheme(theme);
-  };
-  if (theme) {
-    return <Redirect to={`/pageTheme/${theme}`} />;
-  }
+
+
+//  var findClickTheme = (theme) => {
+// setTheme(theme)}
+
+
+    
+  // }
+  // if (theme){
+  //  return <Redirect to={`/pageTheme/${theme}`} />
+  // }
 
   var showModal = () => {
     setIsModalVisible(true);
@@ -80,99 +86,78 @@ function SideBarDroite(props) {
   };
 
   var handleClick = async () => {
-    if (props.token == null) {
-      showModal();
-    } else {
-      setIsConnect(true);
-    }
-  };
+    if (props.token == null){
+    showModal()  
+ } else{
+   setIsConnect(true)
+  }}
 
-  if (isConnect) {
+  if (isConnect){
     return <Redirect to="/pageprofil" />;
   }
 
   var handleClickPubli = (e) => {
-    if (props.token == null) {
-      showModal();
-    } else {
-      setIsConnectProfil(true);
+    if (props.token == null){
+      showModal()  
+   } else{
+     setIsConnectProfil(true)
+    }}
+  
+    if (isConnectProfil){
+      return <Redirect to="/nouvelPublication" />;
     }
-  };
+  
 
-  if (isConnectProfil) {
-    return <Redirect to="/nouvelPublication" />;
-  }
+  
 
-  var publiTheme = themeData.map((theme, i) => {
-    return (
-      <Menu.Item onClick={() => findClickTheme(theme)} key="i">
-        {theme}
-      </Menu.Item>
-    );
-  });
 
+    var publiTheme = themeData.map((theme, i) => {
+      return (
+        
+        <Menu.Item  key="i"> <Link to={`/pageTheme/${theme}`} > {theme}  </Link></Menu.Item>
+      )})
+ 
   return (
     <Sider className="site-layout-background">
-      <Modal
-        title="connexion/inscription"
-        style={{ displayflex: 1, width: 150 }}
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Inscription />{" "}
-      </Modal>{" "}
-      <Affix offsetTop={top}>
-        <Menu
-          style={{ width: 200 }}
-          defaultSelectedKeys={["1"]}
-          // defaultOpenKeys={["sub1"]}
-        >
-          <Menu.Item key="1" icon={<MailOutlined />}>
-            <Link to="/">Accueil</Link>
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<AppstoreOutlined />} title="Thématique">
-            {publiTheme}
-          </SubMenu>
-          <Menu.Item
-            onClick={() => handleClick()}
-            key="2"
-            icon={<CalendarOutlined />}
-          >
-            Mon compte
-          </Menu.Item>
+      <Modal  style={{ displayflex: 1, width: 150 }}
 
-          <Menu.Item
-            onClick={() => handleClickPubli()}
-            key="link"
-            icon={<EditOutlined />}
-          >
-            Nouvelle publication
-          </Menu.Item>
-        </Menu>
-        <div style={{ marginLeft: "40px" }}>
-          <TwitterOutlined
-            style={{ fontSize: "20px", color: "#214C74" }}
-            key="twitter"
-          />
-          <Divider type="vertical" />
-          <FacebookOutlined
-            style={{ fontSize: "20px", color: "#214C74" }}
-            key="facebook"
-          />
-          <Divider type="vertical" />
-          <LinkedinOutlined
-            style={{ fontSize: "20px", color: "#214C74" }}
-            key="linkedin"
-          />
-        </div>
-      </Affix>
+visible={isModalVisible}
+onOk={handleOk}
+onCancel={handleCancel}>
+<Inscription /> </Modal>
+      {" "}
+      <Menu
+        style={{ width: 200 }}
+        defaultSelectedKeys={["1"]}
+        // defaultOpenKeys={["sub1"]}
+      >
+        <Menu.Item key="1" icon={<MailOutlined />}>
+          <Link to="/">Accueil</Link>
+        </Menu.Item>
+        <SubMenu key="sub1" icon={<AppstoreOutlined />} title="Thématique">
+          {publiTheme}
+        </SubMenu>
+        <Menu.Item  onClick={() => handleClick()} key="2" icon={<CalendarOutlined  />}  >
+        Mon compte
+        </Menu.Item>
+
+        <Menu.Item  onClick={() => handleClickPubli()} key="link" icon={<EditOutlined />}>
+          
+        Nouvelle publication
+          
+        </Menu.Item>
+       
+      </Menu>
     </Sider>
   );
 }
 
-function mapStateToProps(state) {
-  return { token: state.token };
+function mapStateToProps(state){
+  return {token:state.token}
 }
 
-export default connect(mapStateToProps, null)(SideBarDroite);
+export default connect(
+  mapStateToProps,
+  null,
+ 
+)(SideBarDroite) ;
