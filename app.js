f819af246e7ba49e88696eb8f036a8287016ff8b
remy <remy@MacBook-Air-de-remy.local>
2021-11-18 11:59:10 +0100

    Heroku

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/publications', publicationsRouter);
